91572f824fd6c5eb660bf48e66ca5ef0df465976
jackweiss11211 <weissjack11211@gmail.com>
2025-01-23 22:01:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const archiver = require('archiver');
-
+const path = require('path');
 const app = express();
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 async function takeScreenshotAndHTML(query) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
